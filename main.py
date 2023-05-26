1a7c207e5404a14e23b5d16b42b66c0f5f8e893e
Benjamin Mazzega <ben.mazzega@gmail.com>
2023-05-26 09:30:15 +0200

    correct indent

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
         elif choice == "3" :
             add_quote(quotes, "quotes.txt")
       
-      elif choice == "4" : # handling display_count()
+        elif choice == "4" : # handling display_count()
             count = int(input("Enter the number of quotes to display: "))
             display_quotes(quotes, count)
 
