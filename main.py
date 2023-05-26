ee46dbf94a77c4a18b172f1a90c57ced1ac5eae2
Josselin <josselin.keib@efrei.net>
2023-05-26 09:02:27 +0200

    revert "first commit"
    this revert commit
    
    :cd

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ def menu():
     print("\n==== Programming Quotes ====")
     print("1. Random quote")
     print("2. All quotes")
-    print("3. Exit")
+    print("3. Add quotes")
+    print("4. Exit")
 
 def main():
     while True:
@@ -17,7 +18,9 @@ def main():
             print_quote(random_quote(quotes))
         elif choice == "2":
             view_quotes(quotes)
-        elif choice == "3":
+        elif choice == "3" :
+            add_quote(quotes)
+        elif choice == "4":
             print("Good bye...")
             break
         else:
