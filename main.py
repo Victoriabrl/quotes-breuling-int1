754a962c13823539f22ba2b09ffce87fda7d0959
Josselin <josselin.keib@efrei.net>
2023-05-26 09:11:20 +0200

    Test, add hello world with the new function

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
         elif choice == "2":
             view_quotes(quotes)
         elif choice == "3" :
-            add_quote(quotes)
+            add_quote(quotes, "quotes.txt")
         elif choice == "4":
             print("Good bye...")
             break
